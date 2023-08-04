a837acdd0786faeb67881c1fd4f3b58a684d45a7
RyoTsuchida <138351310+ryo00415@users.noreply.github.com>
2023-08-04 09:12:22 +0000

    Initial Commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,6 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-    @user = User.find(params[:id])
     @book = Book.new
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
-  get 'relationships/followings'
-  get 'relationships/followers'
   devise_for :users
   root to: 'homes#top'
   get "/home/about" => "homes#about", as: "about"
+
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update]
-  resources :users do
-    resource :relationships, only: [:create, :destroy]
+  resources :users, only: [:index,:show,:edit,:update] do
+  resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
