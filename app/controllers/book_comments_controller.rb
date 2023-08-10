692d87e5283800c72435b0fcd2800d9bd69027cc
RyoTsuchida <138351310+ryo00415@users.noreply.github.com>
2023-08-10 02:01:50 +0000

    Initial Commit

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,24 +1,17 @@
 class BookCommentsController < ApplicationController
-  before_action :authenticate_user!
-
   def create
-    @book = Book.find(params[:book_id])
-    @book_comment = BookComment.new(book_comment_params)
-    @book_comment.book_id = @book.id
-    @book_comment.user_id = current_user.id
-    unless @book_comment.save
-      render 'error'
-    end
+    book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = book.id
+    @comment.save
   end
 
   def destroy
-    @book = Book.find(params[:book_id])
-    book_comment = @book.book_comments.find(params[:id])
-    book_comment.destroy
+    @comment = BookComment.find(params[:id])
+    @comment.destroy
   end
 
   private
-
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,15 @@
 class FavoritesController < ApplicationController
-  before_action :authenticate_user!
-
   def create
-    @book = Book.find(params[:book_id])
-    favorite = @book.favorites.new(user_id: current_user.id)
-    favorite.save
+    book = Book.find(params[:book_id])
+    @favorite = current_user.favorites.new(book_id: book.id)
+    @favorite.save
+    render 'replace_btn'
   end
 
   def destroy
-    @book = Book.find(params[:book_id])
-    favorite = @book.favorites.find_by(user_id: current_user.id)
-    favorite.destroy
+    book = Book.find(params[:book_id])
+    @favorite = current_user.favorites.find_by(book_id: book.id)
+    @favorite.destroy
+    render 'replace_btn'
   end
 end
